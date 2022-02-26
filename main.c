e43461bd87f3da1e628bd03250904c8e3657cbaf
Marek Michalik <marekmichalik43@gmail.com>
2022-02-26 23:49:37 +0100

    Remove fork

--- main.c
+++ main.c
@@ -2,68 +2,56 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-#include <signal.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
 
-#define BUFSIZE 65536 /* larger = more efficient */
-#ifndef SIGCLD
-#define SIGCLD SIGCHLD
-#endif
-
-/* this is a child web server process, so we can exit on errors */
-void web(int fd) {
-    long ret;
-    static char buffer[BUFSIZE + 1]; /* static so zero filled */
-    char s[] = {0xe2, 0x9d, 0xa4, 0xef, 0xb8, 0x8f, 0xf0, 0x9f, 0x8d, 0x8b, 0x00};
-
-    ret = read(fd, buffer, BUFSIZE);    /* read Web request in one go */
-    if (ret == 0 || ret == -1) {    /* read failure stop now */
-        perror("Read");
-    }
-    if (ret > 0 && ret < BUFSIZE)    /* return code is valid chars */
-        buffer[ret] = 0;        /* terminate the buffer */
-    else buffer[0] = 0;
-
-    (void) sprintf(buffer, "HTTP/1.1 200 OK\nContent-Length: %ld\n\n<meta charset=\"utf-8\">\n%s\n", strlen(s) + 24, s); /* Header + a blank line */
-    (void) write(fd, buffer, strlen(buffer));
-
-    sleep(1);    /* allow socket to drain before signalling the socket is closed */
-    close(fd);
-    exit(1);
-}
+#define BUFSIZE 65536
 
 int main() {
-    int port, pid, listenfd, socketfd;
-    socklen_t length;
-    static struct sockaddr_in cli_addr; /* static = initialised to zeros */
-    static struct sockaddr_in serv_addr; /* static = initialised to zeros */
+    int server_fd, client_fd;
+    static struct sockaddr_in client_address;
+    static struct sockaddr_in server_address;
 
-    (void) signal(SIGCLD, SIG_IGN); /* ignore child death */
-    /* setup the network socket */
-    if ((listenfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
+    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
         perror("Socket");
-    port = atoi(getenv("PORT"));
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    serv_addr.sin_port = htons(port);
-    if (bind(listenfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+
+    int one = 1;
+    if ((setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &one, sizeof(int))) < 0)
+        perror("Option");
+
+    int port = atoi(getenv("PORT"));
+    server_address.sin_family = AF_INET;
+    server_address.sin_addr.s_addr = htonl(INADDR_ANY);
+    server_address.sin_port = htons(port);
+
+    if (bind(server_fd, (struct sockaddr *) &server_address, sizeof(server_address)) < 0)
         perror("Bind");
-    if (listen(listenfd, 64) < 0)
+
+    if (listen(server_fd, 64) < 0)
         perror("Listen");
+
     while (1) {
-        length = sizeof(cli_addr);
-        if ((socketfd = accept(listenfd, (struct sockaddr *) &cli_addr, &length)) < 0)
+        socklen_t client_address_length = sizeof(client_address);
+        if ((client_fd = accept(server_fd, (struct sockaddr *) &client_address, &client_address_length)) < 0)
             perror("Accept");
-        if ((pid = fork()) < 0) {
-            perror("Fork");
-        } else {
-            if (pid == 0) {    /* child */
-                (void) close(listenfd);
-                web(socketfd); /* never returns */
-            } else {    /* parent */
-                (void) close(socketfd);
-            }
+
+        long ret;
+        static char buffer[BUFSIZE + 1]; /* static so zero filled */
+
+        ret = read(client_fd, buffer, BUFSIZE);    /* read Web request in one go */
+        if (ret == 0 || ret == -1) {    /* read failure stop now */
+            perror("Read");
         }
+        if (ret > 0 && ret < BUFSIZE)    /* return code is valid chars */
+            buffer[ret] = 0;        /* terminate the buffer */
+        else buffer[0] = 0;
+
+        char emojis[] = {0xe2, 0x9d, 0xa4, 0xef, 0xb8, 0x8f, 0xf0, 0x9f, 0x8d, 0x8b, 0x00};
+        int content_length = strlen(emojis) + 24;
+        sprintf(buffer, "HTTP/1.1 200 OK\nContent-Length: %d\n\n<meta charset=\"utf-8\">\n%s\n", content_length,
+                emojis);
+        write(client_fd, buffer, strlen(buffer));
+
+        close(client_fd);
     }
 }
