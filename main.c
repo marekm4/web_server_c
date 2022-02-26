41edbc7c7cf66e20b79b80b6efe7d335f5c4b195
Marek Michalik <marekmichalik43@gmail.com>
2022-02-26 21:07:02 +0100

    Remove logger and file response

--- main.c
+++ main.c
@@ -18,103 +18,24 @@
 #ifndef SIGCLD
 #define SIGCLD SIGCHLD
 #endif
-struct {
-    char *ext;
-    char *filetype;
-} extensions [] = {
-        {"gif", "image/gif" },  {"jpg", "image/jpg" }, {"jpeg","image/jpeg"},
-        {"png", "image/png" },  {"ico", "image/ico" },  {"zip", "image/zip" },
-        {"gz",  "image/gz"  },  {"tar", "image/tar" },  {"htm", "text/html" },
-        {"html","text/html" },  {0,0} };
-
-void logger(int type, char *s1, char *s2, int socket_fd)
-{
-    int fd ;
-    char logbuffer[BUFSIZE*2];
-
-    switch (type) {
-        case ERROR: (void)sprintf(logbuffer,"ERROR: %s:%s Errno=%d exiting pid=%d",s1, s2, errno,getpid());
-            break;
-        case FORBIDDEN:
-            (void)write(socket_fd, "HTTP/1.1 403 Forbidden\nContent-Length: 185\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>403 Forbidden</title>\n</head><body>\n<h1>Forbidden</h1>\nThe requested URL, file type or operation is not allowed on this simple static file webserver.\n</body></html>\n",271);
-            (void)sprintf(logbuffer,"FORBIDDEN: %s:%s",s1, s2);
-            break;
-        case NOTFOUND:
-            (void)write(socket_fd, "HTTP/1.1 404 Not Found\nContent-Length: 136\nConnection: close\nContent-Type: text/html\n\n<html><head>\n<title>404 Not Found</title>\n</head><body>\n<h1>Not Found</h1>\nThe requested URL was not found on this server.\n</body></html>\n",224);
-            (void)sprintf(logbuffer,"NOT FOUND: %s:%s",s1, s2);
-            break;
-        case LOG: (void)sprintf(logbuffer," INFO: %s:%s:%d",s1, s2,socket_fd); break;
-    }
-    /* No checks here, nothing can be done with a failure anyway */
-    if((fd = open("nweb.log", O_CREAT| O_WRONLY | O_APPEND,0644)) >= 0) {
-        (void)write(fd,logbuffer,strlen(logbuffer));
-        (void)write(fd,"\n",1);
-        (void)close(fd);
-    }
-    if(type == ERROR || type == NOTFOUND || type == FORBIDDEN) exit(3);
-}
 
 /* this is a child web server process, so we can exit on errors */
 void web(int fd, int hit)
 {
-    int j, file_fd, buflen;
-    long i, ret, len;
-    char * fstr;
+    long ret;
     static char buffer[BUFSIZE+1]; /* static so zero filled */
 
     ret =read(fd,buffer,BUFSIZE); 	/* read Web request in one go */
     if(ret == 0 || ret == -1) {	/* read failure stop now */
-        logger(FORBIDDEN,"failed to read browser request","",fd);
+        perror("Read");
     }
     if(ret > 0 && ret < BUFSIZE)	/* return code is valid chars */
         buffer[ret]=0;		/* terminate the buffer */
     else buffer[0]=0;
-    for(i=0;i<ret;i++)	/* remove CF and LF characters */
-        if(buffer[i] == '\r' || buffer[i] == '\n')
-            buffer[i]='*';
-    logger(LOG,"request",buffer,hit);
-    if( strncmp(buffer,"GET ",4) && strncmp(buffer,"get ",4) ) {
-        logger(FORBIDDEN,"Only simple GET operation supported",buffer,fd);
-    }
-    for(i=4;i<BUFSIZE;i++) { /* null terminate after the second space to ignore extra stuff */
-        if(buffer[i] == ' ') { /* string is "GET URL " +lots of other stuff */
-            buffer[i] = 0;
-            break;
-        }
-    }
-    for(j=0;j<i-1;j++) 	/* check for illegal parent directory use .. */
-        if(buffer[j] == '.' && buffer[j+1] == '.') {
-            logger(FORBIDDEN,"Parent directory (..) path names not supported",buffer,fd);
-        }
-    if( !strncmp(&buffer[0],"GET /\0",6) || !strncmp(&buffer[0],"get /\0",6) ) /* convert no filename to index file */
-        (void)strcpy(buffer,"GET /index.html");
 
-    /* work out the file type and check we support it */
-    buflen=strlen(buffer);
-    fstr = (char *)0;
-    for(i=0;extensions[i].ext != 0;i++) {
-        len = strlen(extensions[i].ext);
-        if( !strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
-            fstr =extensions[i].filetype;
-            break;
-        }
-    }
-    if(fstr == 0) logger(FORBIDDEN,"file extension type not supported",buffer,fd);
-
-    if(( file_fd = open(&buffer[5],O_RDONLY)) == -1) {  /* open the file for reading */
-        logger(NOTFOUND, "failed to open file",&buffer[5],fd);
-    }
-    logger(LOG,"SEND",&buffer[5],hit);
-    len = (long)lseek(file_fd, (off_t)0, SEEK_END); /* lseek to the file end to find the length */
-    (void)lseek(file_fd, (off_t)0, SEEK_SET); /* lseek back to the file start ready for reading */
-    (void)sprintf(buffer,"HTTP/1.1 200 OK\nServer: nweb/%d.0\nContent-Length: %ld\nConnection: close\nContent-Type: %s\n\n", VERSION, len, fstr); /* Header + a blank line */
-    logger(LOG,"Header",buffer,hit);
+    (void)sprintf(buffer,"HTTP/1.1 200 OK\n\n"); /* Header + a blank line */
     (void)write(fd,buffer,strlen(buffer));
 
-    /* send file in 8KB block - last block may be smaller */
-    while (	(ret = read(file_fd, buffer, BUFSIZE)) > 0 ) {
-        (void)write(fd,buffer,ret);
-    }
     sleep(1);	/* allow socket to drain before signalling the socket is closed */
     close(fd);
     exit(1);
