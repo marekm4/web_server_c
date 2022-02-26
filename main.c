0901d21aab9bf7c3e748ff117b9c445e87fec3a3
Marek Michalik <marekmichalik43@gmail.com>
2022-02-26 20:42:25 +0100

    Remove daemon

--- main.c
+++ main.c
@@ -122,53 +122,18 @@ void web(int fd, int hit)
 
 int main(int argc, char **argv)
 {
+    char s[] = {0xe2, 0x9d, 0xa4, 0xef, 0xb8, 0x8f, 0xf0, 0x9f, 0x8d, 0x8b, 0x00};
+    printf("%s\n", s);
     int i, port, pid, listenfd, socketfd, hit;
     socklen_t length;
     static struct sockaddr_in cli_addr; /* static = initialised to zeros */
     static struct sockaddr_in serv_addr; /* static = initialised to zeros */
 
-    if( argc < 3  || argc > 3 || !strcmp(argv[1], "-?") ) {
-        (void)printf("hint: nweb Port-Number Top-Directory\t\tversion %d\n\n"
-                     "\tnweb is a small and very safe mini web server\n"
-                     "\tnweb only servers out file/web pages with extensions named below\n"
-                     "\t and only from the named directory or its sub-directories.\n"
-                     "\tThere is no fancy features = safe and secure.\n\n"
-                     "\tExample: nweb 8181 /home/nwebdir &\n\n"
-                     "\tOnly Supports:", VERSION);
-        for(i=0;extensions[i].ext != 0;i++)
-            (void)printf(" %s",extensions[i].ext);
-
-        (void)printf("\n\tNot Supported: URLs including \"..\", Java, Javascript, CGI\n"
-                     "\tNot Supported: directories / /etc /bin /lib /tmp /usr /dev /sbin \n"
-                     "\tNo warranty given or implied\n\tNigel Griffiths nag@uk.ibm.com\n"  );
-        exit(0);
-    }
-    if( !strncmp(argv[2],"/"   ,2 ) || !strncmp(argv[2],"/etc", 5 ) ||
-        !strncmp(argv[2],"/bin",5 ) || !strncmp(argv[2],"/lib", 5 ) ||
-        !strncmp(argv[2],"/tmp",5 ) || !strncmp(argv[2],"/usr", 5 ) ||
-        !strncmp(argv[2],"/dev",5 ) || !strncmp(argv[2],"/sbin",6) ){
-        (void)printf("ERROR: Bad top directory %s, see nweb -?\n",argv[2]);
-        exit(3);
-    }
-    if(chroot(argv[2]) == -1){
-        (void)printf("ERROR: Can't Change to directory %s\n",argv[2]);
-        exit(4);
-    }
-    /* Become deamon + unstopable and no zombies children (= no wait()) */
-    if(fork() != 0)
-        return 0; /* parent returns OK to shell */
     (void)signal(SIGCLD, SIG_IGN); /* ignore child death */
-    (void)signal(SIGHUP, SIG_IGN); /* ignore terminal hangups */
-    for(i=0;i<32;i++)
-        (void)close(i);		/* close open files */
-    (void)setpgrp();		/* break away from process group */
-    logger(LOG,"nweb starting",argv[1],getpid());
     /* setup the network socket */
     if((listenfd = socket(AF_INET, SOCK_STREAM,0)) <0)
         logger(ERROR, "system call","socket",0);
     port = atoi(argv[1]);
-    if(port < 0 || port >60000)
-        logger(ERROR,"Invalid port number (try 1->60000)",argv[1],0);
     serv_addr.sin_family = AF_INET;
     serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     serv_addr.sin_port = htons(port);
